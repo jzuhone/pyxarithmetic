6cf7963effe8984379a0bbc90d862df4f17c91eb
John ZuHone <jzuhone@gmail.com>
2017-01-14 01:15:15 -0500

    Adding FITS info

--- pyxarithmetic/image.py
+++ pyxarithmetic/image.py
@@ -1,28 +1,35 @@
 import numpy as np
 import astropy.io.fits as pyfits
+import astropy.wcs as pywcs
 from six import string_types
 from scipy.interpolate import InterpolatedUnivariateSpline
 
 class ClusterImage(object):
     def __init__(self, source_img, model_img=None):
-        self.source_img = self._get_image(source_img) 
+        self.source_img = self._get_image(source_img, find_wcs=True) 
         if model_img is None:
             self.model_img = self._make_model(self.source_img)
         else:
             self.model_img = self._get_image(model_img)
         self.resid_img = self.source_img/self.model_img-1.0
 
-    def _get_image(self, img):
+    def _get_image(self, img, find_wcs=False):
+        header = None
         if not isinstance(img, np.ndarray):
             if isinstance(img, string_types):
                 f = pyfits.open(img)
                 img = f[0].data
+                header = f[0].header
                 f.close()
             elif isinstance(img, pyfits.HDUList):
+                header = img[0].header
                 img = img[0].data
             elif isinstance(img, pyfits.ImageHDU) or \
                 (isinstance(img, pyfits.PrimaryHDU) and img.is_image):
+                header = img.header
                 img = img.data
+        if find_wcs:
+            self.wcs = pywcs.WCS(header=header, naxis=2)
         return img
 
     def _make_model(self, img):
@@ -38,6 +45,12 @@ class ClusterImage(object):
         rmid = 0.5 * (rbin[1:] + rbin[:-1])
         pr = InterpolatedUnivariateSpline(rmid, p)
         return pr(r)
+
+    def writeto(self, filename, overwrite=True):
+        shdu = pyfits.PrimaryHDU(self.source_img, header=header)
+        mhdu = pyfits.ImageHDU(self.model_img, header=header)
+        rhdu = pyfits.ImageHDU(self.resid_img, header=header)
+        pyfits.HDUList([shdu, mhdu, rhdu]).writeto(filename, overwrite=overwrite)
 
 def add_images(img1, img2, cB1, cB2):
     X = cB1*img1.resid_img+cB2*img2.resid_img
